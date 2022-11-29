5d742e5af72afb4bc66fb2d4958878f8fabcca8a
seangray-dev <seangray.dev@gmail.com>
2022-11-29 07:38:35 -0500

    added javascript for toggle functions

--- app/JS/script.js
+++ app/JS/script.js
@@ -1,1 +1,55 @@
-console.log("hello  world");
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+
+const setColorMode = () => {
+  if (localStorage.getItem("colorMode") == "dark") {
+    setDarkMode();
+    darkButton.click();
+  } else if (localStorage.getItem("colorMode") == "light") {
+    setLightMode();
+    lightButton.click();
+  }
+};
+
+const checkMode = () => {
+  if (localStorage.getItem("colorMode") == null) {
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      lightButton.click();
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      darkButton.click();
+    }
+  }
+};
+
+const checkModeChange = () => {
+  window
+    .matchMedia("prefers-color-scheme: dark")
+    .addEventListener("change", () => {
+      checkMode();
+    });
+};
+
+const setDarkMode = () => {
+  document.querySelector("body").classList = "dark";
+};
+
+const setLightMode = () => {
+  document.querySelector("body").classList = "light";
+};
+
+setColorMode();
+checkMode();
+checkModeChange();
+
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener("click", () => {
+    if (darkButton.checked) {
+      localStorage.setItem("colormode", "dark");
+      setDarkMode();
+    } else {
+      localStorage.setItem("colormode", "light");
+      setLightMode();
+    }
+  });
+}
